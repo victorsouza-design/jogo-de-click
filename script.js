66b7262524b3fe456df5e1ef86076340525d3d89
Victor Hugo Silva <victor.souza@unifebe.edu.br>
2026-06-23 17:44:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document.getElementById("botaoClick").onclick = function() {
+document.getElementById("botaoclique").onclick = function() {
     alert("clicou");
 };
 
@@ -8,7 +8,7 @@ let Valorclique= 1;
 let upgrade= 0;
 
 // clique principal 
-document.getElementById("botaoClick").onclick = function() {
+document.getElementById("botaoclique").onclick = function() {
     pontos =pontos+ Valorclique;
     atualizarTela();
     
@@ -36,3 +36,63 @@ function atualizarTela() {
     document.getElementById("Valorclique"). textContent = Valorclique;
     document.getElementById("upgrades"). textContent = upgrade;
 }
+
+function efeitoClique(x, y) {
+    const ponto = document.createElement("span");
+    ponto.textcontent = "+" + valorclique;
+    ponto.style.positive = "absolute";
+    ponto.style.left = x + "px";
+    ponto.style.top = y + "px";
+    ponto.style.color = "yellow";
+    ponto.style.fontWeight = "bold";
+
+    document.body.appendchild(pontos);
+
+    settimeout(() => {
+        ponto.remove();
+    }, 800);
+}
+
+document.getelementbyil("botaoclique"). oneclick = function(e) {
+    pontos += valorclique;
+
+    efeitoClique(e.clientx, e.clienty);
+
+    atualizartela();
+};
+
+let custoupgrade = 10;
+
+function comprarUpgrade() {
+    if (pontos >= custoupgrade) {
+        pontos -= cuastoupgrade;
+        valorclique += 1;
+        upgrade++;
+
+        custoupgrade = Math.floor(custoupgrade * 1.5);
+
+        atualizartela();
+    }
+}
+
+document.getelementbyil("custoupgrade"). textcontent = custoupgrade;
+
+function atualizarTela() {
+    document.getelementbyil("pontos").textContent = pontos;
+    document.getelementbyil("valorclique").textcontent = valorclique;
+    document.getelementbyil("upgrades").textContent = upgrade;
+    document.getelementbyil("custoupgrade").textcontent = custoupgrade;
+
+    const botao = document.querySelector(".upgrade");
+    botao.Disabled = pontos < custoupgrade;
+}
+
+setInterval(() => {
+    localStorage.getItem("pontods", pontos);
+    localStorage.getItem("valorclique", valorclique);
+    localStorage.getItem("upgrades", upgrade);
+}, 1000);
+
+pontos = parseInt(localStorage.getItem("pontos")) || 0;
+valorclique = parseInt(localStorage.getItem("valorclique")) || 1;
+upgrades = parseInt(localStorage.getItem("upgrades")) || 0;
